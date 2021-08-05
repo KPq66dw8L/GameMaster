9d909df13a3993ca00ea6fe638325d48b8048617
KPq66dw8L <63552897+KPq66dw8L@users.noreply.github.com>
2021-08-05 18:07:25 +0200

    Update primary.js

--- primary.js
+++ primary.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
+
 const Discord = require('discord.js');
 const { token } = require('./config.json')
 const client = new Discord.Client();
