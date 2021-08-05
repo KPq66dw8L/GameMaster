b414f399b7c95d6be3e3a1deed53bc0d953d39a8
KPq66dw8L <63552897+KPq66dw8L@users.noreply.github.com>
2021-08-05 18:00:38 +0200

    nullify this file

--- bot.js
+++ bot.js
@@ -1,28 +1,28 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-  }
+//if (process.env.NODE_ENV !== 'production') {
+//    require('dotenv').config();
+//  }
 
-const Discord = require("discord.js");
+//const Discord = require("discord.js");
 // const fetch = require("node-fetch");
 // const express = require("express");
 // const keepAlive = require("./server");
 
-const client = new Discord.Client();
+//const client = new Discord.Client();
 
-const my_secret = process.env.TOKEN;
+//const my_secret = process.env.TOKEN;
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+//client.on("ready", () => {
+//  console.log(`Logged in as ${client.user.tag}!`)
+//})
 
-client.on("message", msg => {
+//client.on("message", msg => {
   //so that the bot does not respond to itself
-  if(msg.author.bot) return
+//  if(msg.author.bot) return
 
-  if(msg.content === "ping") {
-    msg.reply("pong");
-  }
-});
+//  if(msg.content === "ping") {
+//    msg.reply("pong");
+//  }
+//});
 
 
-client.login(my_secret);
+//client.login(my_secret);
